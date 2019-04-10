a8354fbb6d953103f290926052a93f13cca63ffc
Ashish Barathwal NC5083 <ashishbarathwalnc5083@ninja-its-MacBook-Air.local>
2019-04-10 23:57:06 +0530

    Immutable Queue Implemented

---
+++ src/ImmutableStack.java
@@ -0,0 +1,73 @@
+
+/**
+ * This class is the concrete class that represents an Immutable Stack.
+ * @author asifiqbal
+ *
+ * @param <T> generic type for the elemets of the stack
+ */
+public final class ImmutableStack<T> implements Stack<T>{
+    private final T head;
+    private final Stack<T> tail;
+
+    private ImmutableStack(T head, Stack<T> tail){
+        this.head = head;
+        this.tail = tail;
+    }
+
+    public final Stack<T> push(T t){
+        return new ImmutableStack<T>(t, this);
+    }
+
+    public final Stack<T> pop(){
+        return tail;
+    }
+
+    public final T head(){
+        return head;
+    }
+
+    public final boolean isEmpty(){
+        return false;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public final static Stack getEmptyStack(){
+        return EmptyStack.getInstance();
+    }
+
+    /**
+     * Represents an empty stack. This is a singleton.
+     * @author asifiqbal
+     *
+     * @param <T>
+     */
+    private static final class EmptyStack<T> implements Stack<T>{
+
+        @SuppressWarnings("rawtypes")
+        private final static EmptyStack emptyStack = new EmptyStack();
+
+        @SuppressWarnings("rawtypes")
+        public final static EmptyStack getInstance(){
+            return emptyStack;
+        }
+
+        public final Stack<T> push(T t){
+            return new ImmutableStack<T>(t, this);
+        }
+
+        public final Stack<T> pop(){
+            System.out.println("Stack is empty.");
+            return null;
+        }
+
+        public final T head(){
+            System.out.println("Stack is empty.");
+            return null;
+        }
+
+        public final boolean isEmpty(){
+            return true;
+        }
+    }
+}
+
---
+++ src/Stack.java
@@ -0,0 +1,11 @@
+
+public interface Stack<T> {
+
+    public Stack<T> push(T t);
+
+    public Stack<T> pop();
+
+    public T head();
+
+    public boolean isEmpty();
+}
